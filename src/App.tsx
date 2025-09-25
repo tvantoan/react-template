b1a420a96c61aa424b5b1dc2ff93b574724b82a0
toan <kiiriitoo129@gmail.com>
2025-09-25 07:36:19 +0700

    modify

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="absolute bg-amber-50">ABC</div>;
+}
+
+export default App;
--- src/app/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return <div className="absolute bg-amber-50 ">
-    ABC
-  </div>;
-}
-
-export default App;
